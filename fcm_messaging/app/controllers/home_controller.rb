cd940fb99fdcd46a9bfe2645add8b2f9bf4ab255
akash073 <akash073@gmail.com>
2017-05-08 17:20:11 +0600

    Service name is added for project

--- fcm_messaging/app/controllers/home_controller.rb
+++ fcm_messaging/app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
-  require 'android_messaging/fcm_registration'
+  require 'android_messaging/fcm_registration_service'
   def index
 
     #from api
-    @current_user_id=16
+    @current_user_id=18
     @current_regiatration_token="abc"
 
 
-    @fcm_registration = AndroidMessaging::FcmResgistation.new(@current_user_id,@current_regiatration_token)
+    @fcm_registration = AndroidMessaging::FcmResgistationService.new(@current_user_id,@current_regiatration_token)
 
     @fcm_registration.SetOrUpdateFcmResgitrationToken
 
