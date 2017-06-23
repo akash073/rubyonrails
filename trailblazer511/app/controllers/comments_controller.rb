37c24e4604248edff7449a695dcb6334b4fcef7f
DESKTOP-JNTNCB8\Akash <akash073@gmail.com>
2017-06-23 17:29:24 +0600

    model added with operations

--- trailblazer511/app/controllers/comments_controller.rb
+++ trailblazer511/app/controllers/comments_controller.rb
@@ -24,17 +24,11 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    run Comment::Create do |op|
+      return redirect_to op.model
     end
+
+    render action: :new
   end
 
   # PATCH/PUT /comments/1
