479fcaaf8fc8cd4046ea8dbe8a8ea13976592fe2
DESKTOP-JNTNCB8\Akash <akash073@gmail.com>
2017-06-24 16:38:38 +0600

    trailbalzer operation call is done successfull

--- trailblazer511/app/concepts/comment/operation/create.rb
+++ trailblazer511/app/concepts/comment/operation/create.rb
@@ -1,12 +1,20 @@
 require "trailblazer"
 
 class Comment::Create < Trailblazer::Operation
-  extend Contract::DSL
-  step     Model( Comment, :new )
-  step     :create
-  
-  def create(result)
-    name =  result["params"]["comment"]["name"]
+#  extend Contract::DSL
+
+ # contract do
+    # this is a Reform::Form class!
+ #   property :name, validates: {presence: true}
+ # end
+  step Model( Comment, :new )
+  step :create
+ # step Contract::Build()
+  #step Contract::Validate( key: :comment )
+  #step Contract::Persist( )
+
+  def create(options)
+    name =  options["params"]["comment"]["name"]
     Comment.create!(:name => name)
   end
 end
