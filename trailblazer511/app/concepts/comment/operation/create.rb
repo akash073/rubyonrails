bd2b2e8a0ace82346d31bab296062616c51552e3
DESKTOP-JNTNCB8\Akash <akash073@gmail.com>
2017-06-24 16:24:49 +0600

    Operation added successfully

--- trailblazer511/app/concepts/comment/operation/create.rb
+++ trailblazer511/app/concepts/comment/operation/create.rb
@@ -4,9 +4,9 @@ class Comment::Create < Trailblazer::Operation
   extend Contract::DSL
   step     Model( Comment, :new )
   step     :create
-  # ..
-  def create(options)
-    name =  options["params"]["comment"]["name"]
+  
+  def create(result)
+    name =  result["params"]["comment"]["name"]
     Comment.create!(:name => name)
   end
 end
